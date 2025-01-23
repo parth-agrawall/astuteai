9b6d8e90d5e9f842e087a447d000ad5ad1cd0004
parth-agrawall <yellor92@gmail.com>
2025-01-23 21:56:04 +0530

    updated files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { DM_Sans } from "next/font/google";
 import clsx from "clsx";
 import "./globals.css";
 import ConditionalLayout from "@/components/ConditionalLayout";
+// import { Analytics } from "@vercel/analytics/next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -14,34 +15,37 @@ export const metadata: Metadata = {
   description: "One stop solution for all your Startup needs",
 };
 
-const clerkPublishableKey = "pk_test_YnJhdmUtbWFsYW11dGUtMjMuY2xlcmsuYWNjb3VudHMuZGV2JA";
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider publishableKey={clerkPublishableKey}>
+    <ClerkProvider>
       <html lang="en">
         <head>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
           <title>{metadata.title as string}</title>
           <meta name="description" content={metadata.description as string} />
         </head>
         <body className={clsx(dmSans.className, "antialiased")}>
           <ConditionalLayout>
+            {" "}
             <ThemeProvider
               attribute="class"
               defaultTheme="dark"
               enableSystem
               disableTransitionOnChange
             >
-              {children}
+              {children}{" "}
             </ThemeProvider>
           </ConditionalLayout>
           <Toaster />
+          {/* <Analytics mode="production" /> */}
         </body>
       </html>
     </ClerkProvider>
