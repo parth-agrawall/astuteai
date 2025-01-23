6fa14cd1c63244f0e699800df5a108094d2731b0
parth-agrawall <yellor92@gmail.com>
2025-01-23 21:33:58 +0530

    updated layout.tsx file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { DM_Sans } from "next/font/google";
 import clsx from "clsx";
 import "./globals.css";
 import ConditionalLayout from "@/components/ConditionalLayout";
-// import { Analytics } from "@vercel/analytics/next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -15,8 +14,7 @@ export const metadata: Metadata = {
   description: "One stop solution for all your Startup needs",
 };
 
-// Replace 'your-publishable-key-here' with your actual publishable key or use an environment variable
-const clerkPublishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || "pk_test_YnJhdmUtbWFsYW11dGUtMjMuY2xlcmsuYWNjb3VudHMuZGV2JA";
+const clerkPublishableKey = "pk_test_YnJhdmUtbWFsYW11dGUtMjMuY2xlcmsuYWNjb3VudHMuZGV2JA";
 
 export default function RootLayout({
   children,
@@ -28,10 +26,7 @@ export default function RootLayout({
       <html lang="en">
         <head>
           <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <title>{metadata.title as string}</title>
           <meta name="description" content={metadata.description as string} />
         </head>
@@ -47,7 +42,6 @@ export default function RootLayout({
             </ThemeProvider>
           </ConditionalLayout>
           <Toaster />
-          {/* <Analytics mode="production" /> */}
         </body>
       </html>
     </ClerkProvider>
